555c0ca478504bfe7972331b4449aa1095cea28d
RubensLFerreira <rubenslima000@gmail.com>
2024-08-17 18:14:24 -0300

    add route front

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const { router, dirname } = require('./routes/articleRoutes.js');
 
 const server = express();
-const port = process.env.SERVER_PORT;
-const hostname = process.env.SERVER_HOSTNAME;
+const port = process.env.SERVER_PORT || 8080;
+const hostname = process.env.SERVER_HOSTNAME || '0.0.0.0';
 
 server.use(cors());
 server.use(express.json());
@@ -16,4 +16,5 @@ server.use('/', router);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`web running at http://127.0.0.1:5500/public/index.html`);
 });
